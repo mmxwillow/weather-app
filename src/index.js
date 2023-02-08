6ffa297f36316bfc0ad33bc36a5509ac084d7636
mmxwillow <mxwierzba@gmail.com>
2023-02-08 19:19:45 +0100

    Search bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import fetchData from "./fetch-data";
 
 fetchData('warsaw');
+
+const form = document.querySelector('form');
+const searchInput = document.querySelector('input');
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const searchValue = searchInput.value;
+    fetchData(searchValue);
+})
