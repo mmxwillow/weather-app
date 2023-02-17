521ed636221d7b810321311e38491a97de1f1933
mmxwillow <mxwierzba@gmail.com>
2023-02-17 11:19:33 +0100

    UI

--- src/display-data.js
+++ src/display-data.js
@@ -3,6 +3,8 @@ const description = document.querySelector('.description');
 const feelsLike = document.querySelector('.feels-like');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
+const location = document.querySelector('.location')
+const img = document.querySelector('img');
 const toggle = document.querySelector('input[type="checkbox"]')
 
 let system = 'metric';
@@ -20,17 +22,19 @@ const units = {
 }
 
 export default function displayData(data){
+    system = (toggle.checked) ? 'metric' : 'imperial';
     temp = data;
     const tempUnits = `<sup>${units[system].temp}</sup>`;
 
+    img.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
+    location.innerHTML = `${data.city}, ${data.country}`;
     currentTemp.innerHTML = data[system].temp + tempUnits;
     description.innerHTML = data.description;
-    feelsLike.innerHTML = data[system].feels_like + tempUnits;
-    humidity.innerHTML = `${data.humidity}%`;
-    wind.innerHTML = data[system].wind + units[system].speed;
+    feelsLike.innerHTML = `feels like: ${  data[system].feels_like  }${tempUnits}`;
+    humidity.innerHTML = `humidity: ${data.humidity}%`;
+    wind.innerHTML = `wind: ${data[system].wind} ${units[system].speed}`;
 }
 
 toggle.addEventListener('change', () => {
-    system = (system === 'imperial') ? 'metric' : 'imperial';
     displayData(temp);
 })
